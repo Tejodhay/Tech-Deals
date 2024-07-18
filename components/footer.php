135187faf74da5a724dc4818c8182806d10b36e6
Tejodhay19 <tejodhay04@gmail.com>
2024-07-18 09:38:33 +0530

    Tech-Deals
    Online E-Commerce website

---
+++ components/footer.php
@@ -0,0 +1,41 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>Quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> Home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> About</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> Shop</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> Contact</a>
+      </div>
+
+      <div class="box">
+         <h3>Extra links</h3>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> Login</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> Register</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> Cart</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i> Orders</a>
+      </div>
+
+      <div class="box">
+         <h3>Contact Us.</h3>
+         <a href="tel:9800000000"><i class="fas fa-phone"></i> +91 9642878284</a>
+         <a href="tel:9900000000"><i class="fas fa-phone"></i> +91 8332931583</a>
+         <a href="mailto:techdeals@gmail.com"><i class="fas fa-envelope"></i>techdeals@gmail.com</a>
+         <a href="https://www.google.com/maps/place/SRM+University+AP/@16.4649199,80.5083236,15z/data=!4m6!3m5!1s0x3a35f253b87d44b3:0x591c2967f32abc19!8m2!3d16.4649199!4d80.5083236!16s%2Fg%2F11grvk9yxg?entry=ttu"><i class="fas fa-map-marker-alt"></i> SRM University AP</a>
+      </div>
+
+      <div class="box">
+         <h3>Follow Us</h3>
+         <a href="https://www.facebook.com/profile.php?id=61555290966616&mibextid=ZbWKwL" target="_blank"><i class="fab fa-facebook-f"></i>facebook</a>
+         <a href="https://x.com/TechDeals_AtoZ?t=zJnna-4qZTSAOeqGp92MAg&s=08" target="_blank"><i class="fab fa-twitter"></i>Twitter</a>
+         <a href="https://www.instagram.com/tech_deals_offers?igsh=MXZmdzV5ZmR3dHNzcw==" target="_blank"><i class="fab fa-instagram"></i>Instagram</a>
+         <a href="https://www.linkedin.com/in/tech-deals-amazon-aa84522a9?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank"><i class="fab fa-linkedin"></i>Linkedin</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>TechDeals</span> | all rights reserved!</div>
+
+</footer>
